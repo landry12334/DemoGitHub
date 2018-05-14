6cdd338eadd92b9628ad96c50af2a0b45cfdb323
landry12334 <paulndepo84@gmail.com>
2018-05-14 21:10:02 +0200

    CREATION DU PROJET*

---
+++ src/demogithub/MonInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demogithub;
+
+/**
+ *
+ * @author LANDRY
+ */
+public class MonInterface {
+    
+    public String direBonjour(){
+        return "je vous dit bonjour";
+    }
+    
+   
+    
+}
